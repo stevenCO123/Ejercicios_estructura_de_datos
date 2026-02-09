57f16fcd00c5af8235be4f147cdca356ae17130a
stevenCO123 <stevencifu330@outlook.com>
2026-02-09 18:54:28 -0500

    solve exercise 2

--- ejercicio_2/App.java
+++ ejercicio_2/App.java
@@ -2,3 +2,13 @@
 *Ejercicio 2
 *Escribir un programa que pregunte al usuario su edad, y luego le diga cuántos años tendrá en 10 años.
 */
+import java.util.Scanner;
+public class App{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        byte edad_usuario;
+        System.out.println("hola usuario cual es tu edad?");
+        edad_usuario = sc.nextByte();
+        System.out.println("en 10 años tendras " + (edad_usuario+10) + " años");
+    }
+}
