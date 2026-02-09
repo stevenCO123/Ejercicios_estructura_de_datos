c1c93c55bde6d22a60cf1f94d1728c6df7d30a9d
stevenCO123 <stevencifu330@outlook.com>
2026-02-09 18:49:32 -0500

    solve exercise 1

--- ejercicio_1/App.java
+++ ejercicio_1/App.java
@@ -2,8 +2,13 @@
 *Ejercicio 1
 *Escribir un programa que pregunte al usuario su nombre, y luego lo salude.
 */
+import java.util.Scanner;
 public class App{
     public static void main(String[] args){
-        System.out.println("hello word!");
+        Scanner sc = new Scanner(System.in);
+        String nombre_usuario;
+        System.out.println("cual es tu nombre: ");
+        nombre_usuario = sc.nextLine();
+        System.out.println("hola " + nombre_usuario +" !!!");
     }
 }
