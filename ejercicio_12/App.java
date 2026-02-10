56a66b0736833cdffe565dfe851805a192006ca2
stevenCO123 <stevencifu330@outlook.com>
2026-02-10 17:55:08 -0500

    solve exercise 12

--- ejercicio_12/App.java
+++ ejercicio_12/App.java
@@ -5,3 +5,18 @@
 *  se pide realizar un algoritmo que intercambie los valores de ambas variables y
 *  muestre cuanto valen al final las dos variables.
 */
+import java.util.Scanner;
+public class App{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int variableA, variableB, contenedor=0;
+        System.out.println("ingrese el valor entero de la variable A");
+        variableA = sc.nextInt();
+        System.out.println("ingreese el valor entero de la variable B");
+        variableB = sc.nextInt();
+        contenedor = variableA;
+        variableA = variableB;
+        variableB = contenedor;
+        System.out.println("variable A: " + variableA + "\nvariable B: " + variableB);
+    }
+}
