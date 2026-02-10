cd75c9e395f6653b1e612d12564e0a0d65776a29
stevenCO123 <stevencifu330@outlook.com>
2026-02-09 19:30:49 -0500

    solve exercise 5

--- ejercicio_5/App.java
+++ ejercicio_5/App.java
@@ -2,3 +2,16 @@
 *Ejercicio 5
 *Dados dos números, mostrar la suma, resta, división y multiplicación de ambos.
 */
+import java.util.Scanner;
+public class App{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double numero_1, numero_2;
+        System.out.println("ingrese el primer numero");
+        numero_1 = sc.nextDouble();
+        System.out.println("ingrese el segundo numero");
+        numero_2 = sc.nextDouble();
+        System.out.println("suma = " + (numero_1+numero_2) + "\nresta = " + (numero_1 - numero_2) +
+                "\ndivision = " + (numero_1/numero_2) + "\nmultiplicacion = " + (numero_1*numero_2));
+    }
+}
