9311b4230ffc0edd66efdf54d0f11d7915d2c614
stevenCO123 <stevencifu330@outlook.com>
2026-02-10 18:25:14 -0500

    solve exercise 16

--- ejercicio_16/App.java
+++ ejercicio_16/App.java
@@ -2,3 +2,18 @@
 *Ejercicio 16
 *Escribe un programa que lea un número e indique si es par o impar.
 */
+import java.util.Scanner;
+public class App{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int numero;
+        System.out.println("ingrese un numero");
+        numero = sc.nextInt();
+        if(numero % 2 == 0){
+            System.out.println("el numero es par");
+        }
+        else{
+            System.out.println("el numero es impar");
+        }
+    }
+}
