7ce3a9b747b8eacdebb90527fe58b5de1b09f21e
stevenCO123 <stevencifu330@outlook.com>
2026-02-10 18:19:21 -0500

    solve exercise 14

--- ejercicio_14/App.java
+++ ejercicio_14/App.java
@@ -2,3 +2,20 @@
 *Ejercicio 14
 *Algoritmo que pida dos números e indique si el primero es mayor que el segundo.
 */
+import java.util.Scanner;
+public class App{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int numero1,numero2;
+        System.out.println("ingrese el primer numero");
+        numero1 = sc.nextInt();
+        System.out.println("ingrese el segundo numero");
+        numero2 = sc.nextInt();
+        if(numero1>numero2){
+            System.out.println("el primer numero introducido es mayor al segundo");
+        }
+        else {
+            System.out.println("el primer numero introducido no es mayor al segundo");
+        }
+    }
+}
