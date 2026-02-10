0fa68537fa741f38879f993bff411c37636d8ca1
stevenCO123 <stevencifu330@outlook.com>
2026-02-10 16:36:48 -0500

    solve exercise 6

--- ejercicio_6/App.java
+++ ejercicio_6/App.java
@@ -2,3 +2,14 @@
 *Ejercicio 6
 *Escribir un programa que convierta un valor dado en grados Fahrenheit a grados Celsius.
 */
+import java.util.Scanner;
+public class App{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double grados_F, grados_C;
+        System.out.println("ingrese el valor en grados fahrenheit");
+        grados_F = sc.nextDouble();
+        grados_C = (grados_F - 32) * 5/9;
+        System.out.println("en grados Celsius equivale a: " + grados_C);
+    }
+}
