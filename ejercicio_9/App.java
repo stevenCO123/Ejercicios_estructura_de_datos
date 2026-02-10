46e6a341810c18486d7d2112d31afe068c8d4077
stevenCO123 <stevencifu330@outlook.com>
2026-02-10 17:12:34 -0500

    solve exercise 9

--- ejercicio_9/App.java
+++ ejercicio_9/App.java
@@ -3,3 +3,13 @@
 *Una tienda ofrece un descuento del 15% sobre el total de la compra y
 *  un cliente desea saber cuanto deberá pagar finalmente por su compra.
 */
+import java.util.Scanner;
+public class App{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double precio_base;
+        System.out.println("ingrese el precio base de la compra");
+        precio_base = sc.nextDouble();
+        System.out.println("el precio con el 15% de descuento aplicado es: " + (precio_base-(precio_base*0.15)));
+    }
+}
