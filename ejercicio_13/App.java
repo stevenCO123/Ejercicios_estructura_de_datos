260178fe6dd4d1e17cea190cf6b2aca1aec91982
stevenCO123 <stevencifu330@outlook.com>
2026-02-10 18:16:28 -0500

    solve exercise 13

--- ejercicio_13/App.java
+++ ejercicio_13/App.java
@@ -6,3 +6,18 @@
 *  sus respectivas velocidades (km/h) y
 * con esto determinar y mostrar en que tiempo (minutos) alcanzará el vehículo más rápido al otro
 */
+import java.util.Scanner;
+public class App{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double velocidad1, velocidad2, distancia,tiempo;
+        System.out.println("ingrese la velocidad del auto de adelante");
+        velocidad1 = sc.nextDouble();
+        System.out.println("ingrese la velocidad del auto de atras");
+        velocidad2 = sc.nextDouble();
+        System.out.println("ingrese la distancia entre los coches");
+        distancia = sc.nextDouble();
+        tiempo = distancia/(velocidad2-velocidad1)*60;
+        System.out.println("el tiempo que se demora en alcanzarlo es: " + tiempo);
+    }
+}
