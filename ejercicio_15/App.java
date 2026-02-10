16a2d2a5400e116a1f19ebc621613a6cef1644ad
stevenCO123 <stevencifu330@outlook.com>
2026-02-10 18:22:51 -0500

    solve exercise 15

--- ejercicio_15/App.java
+++ ejercicio_15/App.java
@@ -2,3 +2,20 @@
 *Ejercicio 15
 *Algoritmo que pida un número y diga si es positivo, negativo o 0.
 */
+import java.util.Scanner;
+public class App{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int numero;
+        System.out.println("ingrese un numero");
+        numero = sc.nextInt();
+        if(numero>0){
+            System.out.println("el numero es positivo");
+        } else if (numero < 0) {
+            System.out.println("el numero es negativo");
+        }
+        else{
+            System.out.println("el numero es 0");
+        }
+    }
+}
