fa084b90b2e9b9bb9c8296cc16a5ee3597496996
stevenCO123 <stevencifu330@outlook.com>
2026-02-11 18:45:08 -0500

    solve exercise 21

--- ejercicio_21/App.java
+++ ejercicio_21/App.java
@@ -4,3 +4,17 @@
 *Nota: un año es bisiesto si es un número divisible por 4,
 *  pero no si es divisible por 100, excepto que también sea divisible por 400.
 */
+import java.util.Scanner;
+public class App{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int año
+        System.out.println("ingrese un año para saber si es bisiesto o no");
+        año = sc.nextInt();
+        if ((año % 4 == 0 && año % 100 != 0) || (año % 400 == 0)) {
+            System.out.println("es bisiesto");
+        } else {
+            System.out.println("no es bisiesto");
+        }
+    }
+}
