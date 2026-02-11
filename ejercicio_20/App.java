a5d41e7158ec384a673610a1cf81eb4ada14c3a3
stevenCO123 <stevencifu330@outlook.com>
2026-02-11 18:39:53 -0500

    solve exercise 20

--- ejercicio_20/App.java
+++ ejercicio_20/App.java
@@ -8,3 +8,52 @@
 *Si los 3 lados son iguales entonces es equilátero.
 *Si no se cumple ninguna de las condiciones anteriores, es escaleno.
 */
+import java.util.Scanner;
+public class App{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double lado1,lado2,lado3, mayor, cateto1, cateto2;
+        System.out.println("indentificador de triangulos");
+        System.out.println("ingrese el primer lado del triangulo: ");
+        lado1 = sc.nextDouble();
+
+        System.out.println("ingrese el segundo lado del triangulo: ");
+        lado2 = sc.nextDouble();
+
+        System.out.println("ingrese el tercer lado del triangulo: ");
+        lado3 = sc.nextDouble();
+        if (lado1 + lado2 <= lado3 || lado1 + lado3 <= lado2 || lado2 + lado3 <= lado1) {
+            System.out.println("Los valores ingresados no forman un triángulo.");
+            return;
+        }
+
+        mayor = Math.max(lado1, Math.max(lado2, lado3));
+
+        if (mayor == lado1) {
+            cateto1 = lado2;
+            cateto2 = lado3;
+        } else if (mayor == lado2) {
+            cateto1 = lado1;
+            cateto2 = lado3;
+        } else {
+            cateto1 = lado1;
+            cateto2 = lado2;
+        }
+
+        if (Math.pow(cateto1, 2) + Math.pow(cateto2, 2) == Math.pow(mayor, 2)) {
+            System.out.println("Es un triángulo rectángulo.");
+        }
+
+        else if (lado1 == lado2 && lado2 == lado3) {
+            System.out.println("Es un triángulo equilátero.");
+        }
+        else if (lado1 == lado2 || lado1 == lado3 || lado2 == lado3) {
+            System.out.println("Es un triángulo isósceles.");
+        }
+
+        else {
+            System.out.println("Es un triángulo escaleno.");
+        }
+
+    }
+}
