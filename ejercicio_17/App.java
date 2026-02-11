5656727c417ebf3c1961ad5dd2fa2827b10adf25
stevenCO123 <stevencifu330@outlook.com>
2026-02-11 18:12:00 -0500

    solve exercise 17

--- ejercicio_17/App.java
+++ ejercicio_17/App.java
@@ -3,3 +3,34 @@
 *Escribe un programa que pida un nombre de usuario y una contraseña y
 *  si se ha introducido correctamente indicar "Has entrado al sistema", sino se da un mensaje de error.
 */
+import java.util.Scanner;
+public class App{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String nombre_usuario, contraseña, input_usuario, input_contraseña;
+        boolean indicador;
+        System.out.println("hola usuario crea una cuenta");
+        System.out.println("ingrese el nombre de usuario");
+        nombre_usuario = sc.nextLine();
+        System.out.println("ingrese la contraseña");
+        contraseña = sc.nextLine();
+        System.out.println("ahora inicia sesion");
+        do{
+            System.out.println("nombre de usuario");
+            input_usuario = sc.nextLine();
+            System.out.println("contraseña");
+            input_contraseña = sc.nextLine();
+            if(nombre_usuario.equals(input_usuario) && contraseña.equals(input_contraseña)){
+                System.out.println("has entrado al sistema");
+                indicador = false;
+            }
+            else {
+                System.out.println("informacion incorrecta, vuelva a intentarlo");
+                indicador = true;
+            }
+        }while(indicador);
+
+
+
+    }
+}
