da3dd0226170fad42cfdb5627de72d4d64217b3c
stevenCO123 <stevencifu330@outlook.com>
2026-02-12 17:51:51 -0500

    solve exercise 27

--- ejercicio_27/App.java
+++ ejercicio_27/App.java
@@ -4,3 +4,16 @@
 * enteros entre 1 y el propio número y se representa por el número seguido de un signo de exclamación.
 * Por ejemplo 5! = 1x2x3x4x5=120),
 */
+import java.util.Scanner;
+public class App{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int input, resultado=1;
+        System.out.println("ingrese el numero que desea conocer su factorial");
+        input = sc.nextInt();
+        for (int i = 1; i <= input; i++) {
+            resultado = resultado * i;
+        }
+        System.out.println("factorial: " + resultado);
+    }
+}
