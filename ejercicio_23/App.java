0c4cc2bb24d6776c05a2baa124193e24cd9e3929
stevenCO123 <stevencifu330@outlook.com>
2026-02-12 17:23:05 -0500

    solve exercise 23

--- ejercicio_23/App.java
+++ ejercicio_23/App.java
@@ -8,3 +8,40 @@
 *Si es de tipo B, se rebajan 30 céntimos cuando es de tamaño 1, y 50 céntimos cuando es de tamaño 2.
 *Realice un algoritmo para determinar la ganancia obtenida.
 */
+import java.util.Scanner;
+public class App{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double precioInicial, kilos, ajuste, precioFinal, ganancia;
+        int tamaño;
+        String tipo;
+        System.out.println("ingrese el precio inicial de la uva");
+        precioInicial = sc.nextDouble();
+        System.out.println("ingrese el peso (en kilos)");
+        kilos = sc.nextDouble();
+        System.out.println("ingrese el tipo de uva (A o B)");
+        sc.nextLine();
+        tipo = sc.nextLine();
+        System.out.println("ingrese el tipo de tamaño (1 o 2)");
+        tamaño = sc.nextInt();
+        ajuste = 0;
+
+        if (tipo.equals("A")) {
+            if (tamaño == 1) {
+                ajuste = 0.20;
+            } else if (tamaño == 2) {
+                ajuste = 0.30;
+            }
+        } else if (tipo.equals("B")) {
+            if (tamaño == 1) {
+                ajuste = -0.30;
+            } else if (tamaño == 2) {
+                ajuste = -0.50;
+            }
+        }
+
+        precioFinal = precioInicial + ajuste;
+        ganancia = precioFinal * kilos;
+        System.out.println("el precio final es: " + precioFinal + "\nla ganancia es de: " + ganancia);
+    }
+}
