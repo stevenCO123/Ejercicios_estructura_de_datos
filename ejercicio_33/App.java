4547b2f780c4e5a83c5b599082d636be1ccc7677
stevenCO123 <stevencifu330@outlook.com>
2026-02-12 16:19:22 -0500

    solve exercise 33

--- ejercicio_33/App.java
+++ ejercicio_33/App.java
@@ -3,3 +3,23 @@
 *Escribe un programa que diga si un número introducido por teclado es o no primo. Un número primo es aquel
 *que sólo es divisible entre él mismo y la unidad.
 */
+import java.util.Scanner;
+public class App{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int input, contador = 0;
+        System.out.println("ingrese un numero para saber si es primo");
+        input = sc.nextInt();
+        for(int i = 2; i<input; i++ ){
+            if(input % i == 0){
+                contador++;
+            }
+        }
+        if (input == 1 || contador != 0){
+            System.out.println("no es primo");
+        }
+        else {
+            System.out.println("es primo");
+        }
+    }
+}
