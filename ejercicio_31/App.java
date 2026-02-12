f4c646dd73b75a0a8f8673d56a265bc72e7a2def
stevenCO123 <stevencifu330@outlook.com>
2026-02-12 16:33:58 -0500

    solve exercise 31

--- ejercicio_31/App.java
+++ ejercicio_31/App.java
@@ -2,3 +2,20 @@
 *Ejercicio 31
 *Escribir un programa que imprima todos los números pares entre dos números que se le pidan al usuario.
 */
+import java.util.Scanner;
+public class App{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int numeroA, numeroB;
+        System.out.println("ingrese el primer numero");
+        numeroA = sc.nextInt();
+        System.out.println("ingrese el segundo numero");
+        numeroB = sc.nextInt();
+        System.out.println("numeros pares entre estos dos numeros:");
+        for (int i = numeroA+1; i < numeroB; i++) {
+            if(i % 2 == 0){
+                System.out.println(i);
+            }
+        }
+    }
+}
