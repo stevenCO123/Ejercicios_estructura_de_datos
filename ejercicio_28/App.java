2974b6ec4fa828372ae153bd1dda78b72914609b
stevenCO123 <stevencifu330@outlook.com>
2026-02-12 17:56:51 -0500

    solve exercise 28

--- ejercicio_28/App.java
+++ ejercicio_28/App.java
@@ -6,3 +6,43 @@
 * el número (además te dice en cuantos intentos lo has acertado), si se llega al limite de intentos te muestra
 * el número que había generado.
 */
+import java.util.Scanner;
+import java.util.Random;
+public class App{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Random random = new Random();
+
+        int numeroSecreto;
+        int intento;
+        int intentosMaximos;
+        int intentosUsados;
+        int intentosRestantes;
+        boolean acertado;
+
+        numeroSecreto = random.nextInt(100) + 1;
+        intentosMaximos = 10;
+        intentosUsados = 0;
+        acertado = false;
+
+        while (intentosUsados < intentosMaximos && !acertado) {
+
+            intento = sc.nextInt();
+            intentosUsados++;
+            intentosRestantes = intentosMaximos - intentosUsados;
+
+            if (intento == numeroSecreto) {
+                acertado = true;
+                System.out.println("Correcto en " + intentosUsados + " intentos");
+            } else if (intento < numeroSecreto) {
+                System.out.println("Mayor. Intentos restantes: " + intentosRestantes);
+            } else {
+                System.out.println("Menor. Intentos restantes: " + intentosRestantes);
+            }
+        }
+
+        if (!acertado) {
+            System.out.println("Perdiste. El numero era: " + numeroSecreto);
+        }
+    }
+}
